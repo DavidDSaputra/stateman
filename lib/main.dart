d52ece375152593332783b20d18e47aa2c42a756
DavidDSaputra <davidsaputra2704@gmail.com>
2026-02-27 19:09:00 +0700

    wrap app with ChangeNotifierProvider to share counter state

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'counter_model.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
