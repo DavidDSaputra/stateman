93755d751644331d609fd460e9f79e738fe8b438
DavidDSaputra <davidsaputra2704@gmail.com>
2026-02-27 19:08:45 +0700

    extends ChangeNotifier untuk auto notifikasi state changes dari flutter foundation package

--- lib/counter_model.dart
+++ lib/counter_model.dart
@@ -1,9 +1,12 @@
-class CounterModel {
+import 'package:flutter/foundation.dart';
+
+class CounterModel extends ChangeNotifier {
   int _counter = 0;
 
   int get counter => _counter;
 
   void increment() {
     _counter++;
+    notifyListeners();
   }
 }
